0ba8bcebc0d4958366a9a4584b50d4f62c5507fd
Tiago-sendra <166430107+Tiago-sendra@users.noreply.github.com>
2024-04-08 14:28:15 -0300

    Add files via upload

---
+++ Tp 2 sendra (b).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+float sale, tax;
+float tax_rate = 0;
+sale = 22,54;
+tax = sale * tax_rate;
+
+cout<<"the sales is "<<"1,8032"<<endl;
+return 0;
+}
