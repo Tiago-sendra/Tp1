8ee79af3c5d3639abc85a47a443604469a1706e6
Tiago-sendra <166430107+Tiago-sendra@users.noreply.github.com>
2024-04-08 14:44:59 -0300

    Add files via upload

---
+++ Tp 6 Sendra.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int A;
+	int B;
+	
+	cout<<"ingrese digito A"<<endl;
+	cin>>A;
+	
+	cout<<"ingrese digito B"<<endl;
+	cin>>B;
+	
+	if (A >= B){cout<<"A es el digito mas alto"<<endl;}
+	else {cout<<"B es el digito mas alto"<<endl;}
+	
+	return 0;
+}
