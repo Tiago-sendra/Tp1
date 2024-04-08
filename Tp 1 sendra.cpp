57c6d02a99831f77848cbe51edd3282373e580ea
Tiago-sendra <166430107+Tiago-sendra@users.noreply.github.com>
2024-04-08 14:13:58 -0300

    Add files via upload

---
+++ Tp 1 sendra.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int B;
+	int A;
+	int F;
+	
+	cout<<"ingrese la base"<<endl;
+	cin>>B;
+	
+	cout<<"ingrese la altura"<<endl;
+	cin>>A;
+	
+	cout<<"resultado = Area"<<endl;
+	F = B * A;
+	cout<<F<<endl;
+}
