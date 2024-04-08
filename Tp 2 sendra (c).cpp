a2d80c4d604a356264f50d88bcf755eb513107ec
Tiago-sendra <166430107+Tiago-sendra@users.noreply.github.com>
2024-04-08 14:37:54 -0300

    Add files via upload

---
+++ Tp 2 sendra (c).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+float PI = 3.14159;
+float radius = 5;
+float area;
+area = radius * radius * PI;
+cout<<area<<endl;
+radius = 20;
+area = radius * radius * PI;
+cout<<area<<endl;
+
+cout<<"siendo el primer valor de Radius 5"<<endl;
+cout<<"y segundo el 20"<<endl;
+cout<<"siendo su total de apariciones por pantalla 2"<<endl;
+
+return 0;
+}
