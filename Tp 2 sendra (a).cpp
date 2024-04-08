0f9c4ee7b7346a346a3033b5de37f01225d67de6
Tiago-sendra <166430107+Tiago-sendra@users.noreply.github.com>
2024-04-08 14:23:17 -0300

    Add files via upload

---
+++ Tp 2 sendra (a).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+int i,j,c;
+int x;
+i = 4;
+j = i+7;
+
+x = 9;
+x = x*4;
+cout<<"el resultado de I es "<<i<<endl;
+cout<<"el resultado de J es "<<j<<endl;
+cout<<"el resultado de C es "<<c<<endl;
+cout<<"el resultado de X es "<<x<<endl;
+}
